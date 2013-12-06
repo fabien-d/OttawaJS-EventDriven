60f3d3d1052dcec2fd237b95f62d0f33c20df9bc
Fabien Doiron <fabien.doiron@gmail.com>
2013-12-06 09:22:49 -0500

    slide progress

--- slides/ow/custom.event.js
+++ slides/ow/custom.event.js
@@ -4,6 +4,16 @@ document.addEventListener( 'user/create', function ( event ) {
     resultEL.innerHTML = event.detail.username;
 } );
 
+// another.file.js
+document.addEventListener( 'user/create', function ( event ) {
+    console.log( 'Event user/create triggered with: ' + event.detail.username );
+} );
+
+// another.file.again.js
+document.addEventListener( 'user/create', function ( event ) {
+    alert( 'Hey OttawaJS ' + event.detail.username + ' says hi!' );
+} );
+
 // user.js
 var customEl = document.getElementById( 'custom' );
 customEl.addEventListener( 'click', function ( event ) {
